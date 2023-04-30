c79a854efc63e5aeb6c89e6770d6c77af62c978b
Yair Lakretz <yair.lakretz@gmail.com>
2023-04-30 03:09:44 +0200

    with quantifiers and agreement on animate

--- code/stimulus_generator_from_CFG.py
+++ code/stimulus_generator_from_CFG.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Apr 24 22:53:26 2023
 
-@author: yair
-"""
 
+from utils import add_features_to_dict
 from nltk.parse import load_parser
 from nltk.parse.generate import generate
 import pandas as pd
@@ -15,57 +12,30 @@ fn_output = '../stimuli/stimuli_from_fcfg.csv'
 fcfg = load_parser(fn_grammar, trace=1)
 
 
-def init_features_dict():
-    d = {}
-    # subj
-    d['subj_type'] = None
-    d['subj_NUM'] = None
-    d['subj_GEN'] = None
-    
-    # obj
-    d['obj_type'] = None
-    d['obj_NUM'] = None
-    d['obj_GEN'] = None
-    
-    # v
-    d['verb_TENSE'] = None
-    return d
-
-
-def add_features_to_dict(d, pos):
-    for i_item, (key, val) in enumerate(pos.items()):
-        if i_item == 0: # Get subj/verb/object from *type* of pos
-            if '_' in val:
-                svo, svo_type = val.split('_')
-                if svo in ['subj', 'obj']:
-                    d[f'{svo}_type'] = svo_type
-            elif val.endswith('V'):
-                svo = 'verb'
-            else: # determiner
-                svo = None
-        else: # Features of pos
-            if svo is not None:
-                feature_name = key
-                d[f'{svo}_{feature_name}'] = val
-    return d
-
 # MAIN
 list_dicts = []
 with open(fn_output, 'w') as f:
-    for s in list(generate(fcfg.grammar())):
+    for s in list(generate(fcfg.grammar())): # generate all sentences from grammar
         for tree in fcfg.parse(s): # enter loop only if parsable
-            
-            d = init_features_dict()
+            d = {}
             d['sentence'] = ' '.join(s)
-            
             for pos in tree.pos(): # Loop over part of speech
-                d = add_features_to_dict(d, pos[1])
-        list_dicts.append(d)
+                d = add_features_to_dict(d, pos)
+                
+            list_dicts.append(d)
+            continue # skip if there are more possible parses (n_trees>1)
 
 # To dataframe
-df_sentences = pd.DataFrame(list_dicts)  
+df_sentences = pd.DataFrame(list_dicts)
+
+# Move sentence to front column
 cols = sorted(list(df_sentences))
 cols.insert(0, cols.pop(cols.index('sentence')))
 df_sentences = df_sentences.loc[:, cols]
+
+# Remove cases where subject and object are the same (e.g., the dog watches the dog)
+df_sentences = df_sentences.query("subj!=obj")
+
+# Print and save
 print(df_sentences)
 df_sentences.to_csv(fn_output)
---
+++ code/utils.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def add_features_to_dict(d, pos_tuple):
+    word, pos = pos_tuple
+    for i_item, (key, val) in enumerate(pos.items()):
+        if i_item == 0: # Get subj/verb/object from *type* of pos
+            svo = None
+            if '_' in val:
+                svo, svo_type = val.split('_')
+                if svo_type in ['Quantifier', 'Det']: # Exception
+                    d[val] = word # val:subj_QuantifierTrue
+                else:
+                    d[f'{svo}_type'] = svo_type
+                    d[f'{svo}'] = word
+        else: # Features of pos
+            if svo is not None:
+                feature_name = key
+                d[f'{svo}_{feature_name}'] = val
+    return d
