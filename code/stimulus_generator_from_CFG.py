ab17dba170c43121931e30176408c46b7d52a53b
Yair Lakretz <yair.lakretz@gmail.com>
2023-05-03 18:20:07 +0200

    added: embeddings, simple questions; RCs unified; with sentential features

--- code/stimulus_generator_from_CFG.py
+++ code/stimulus_generator_from_CFG.py
@@ -18,9 +18,16 @@ fcfg = load_parser(fn_grammar, trace=1)
 list_dicts = []
 with contextlib.redirect_stdout(open(os.devnull, "w")): # disable NLTK prints
     for s in tqdm(list(generate(fcfg.grammar()))): # generate all sentences from grammar
-        for tree in fcfg.parse(s): # enter loop only if parsable
+        for tree in fcfg.parse(s): # enter loop only if parsablei
             d = {}
             d['sentence'] = ' '.join(s)
+            
+            # extract sentence features from tree label
+            for i_item, (feature, val) in enumerate(tree.label().items()):
+                if i_item > 0:
+                    d[f'sentence_{feature}'] = val # add sentenial features
+                    
+            # extract word features from tree pos
             for pos in tree.pos(): # Loop over part of speech
                 d = add_features_to_dict(d, pos)
 
@@ -37,8 +44,8 @@ df_sentences = df_sentences.loc[:, cols]
 
 # Remove cases where subject and object are the same (e.g., the dog watches the dog)
 # TODO: should be done at the lemma level
-df_sentences = df_sentences.query("subj!=obj")
-df_sentences = df_sentences.query("subj!=embed")
+#df_sentences = df_sentences.query("subj!=obj")
+#df_sentences = df_sentences.query("subj!=embed")
 
 # Print and save
 print(df_sentences)
