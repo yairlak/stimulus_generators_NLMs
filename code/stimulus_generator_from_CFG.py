24e9abdc184d1f4acd6cc18ee57b6e92df5593b0
Yair Lakretz <yair.lakretz@gmail.com>
2023-05-04 09:37:25 +0200

    improved GROUP feature percolation

--- code/stimulus_generator_from_CFG.py
+++ code/stimulus_generator_from_CFG.py
@@ -9,43 +9,38 @@ from nltk.parse.generate import generate
 from tqdm import tqdm
 import pandas as pd
 
-path2grammars = 'grammars/'
+path2grammar = 'grammars/grammar.fcfg'
 grammars = ['SVO', 'embeddings']
 
 fn_output = '../stimuli/stimuli_from_fcfg.csv'
 
 # MAIN
-d_grammars = []
-for grammar in grammars:
-    fn_grammar = f'grammar_{grammar}.fcfg'
-    fcfg = load_parser(os.path.join(path2grammars, fn_grammar), trace=1)
-    
-    d_grammar = []
-    with contextlib.redirect_stdout(open(os.devnull, "w")): # disable NLTK prints
-        for s in tqdm(list(generate(fcfg.grammar()))): # generate all sentences from grammar
-            for tree in fcfg.parse(s): # enter loop only if parsablei
-                d = {}
-                d['sentence'] = ' '.join(s)
-                
-                # extract sentence features from tree label
-                for i_item, (feature, val) in enumerate(tree.label().items()):
-                    if i_item == 0:
-                        pass
-                    else:
-                        d[f'sentence_{feature}'] = val # add sentenial features
-                        
-                # extract word features from tree pos
-                for pos in tree.pos(): # Loop over part of speech
-                    d = add_features_to_dict(d, pos)
-    
-                d_grammar.append(d)
-                continue # skip if there are more possible parses (n_trees>1)
-    
-    d_grammars.extend(d_grammar)
-    print(pd.DataFrame(d_grammar))
-    
+
+fcfg = load_parser(path2grammar, trace=1)
+
+d_grammar = []
+with contextlib.redirect_stdout(open(os.devnull, "w")): # disable NLTK prints
+    for s in tqdm(list(generate(fcfg.grammar()))): # generate all sentences from grammar
+        for tree in fcfg.parse(s): # enter loop only if parsablei
+            d = {}
+            d['sentence'] = ' '.join(s)
+            
+            # extract sentence features from tree label
+            for i_item, (feature, val) in enumerate(tree.label().items()):
+                if i_item == 0:
+                    pass
+                else:
+                    d[f'sentence_{feature}'] = val # add sentenial features
+                    
+            # extract word features from tree pos
+            for pos in tree.pos(): # Loop over part of speech
+                d = add_features_to_dict(d, pos)
+
+            d_grammar.append(d)
+            continue # skip if there are more possible parses (n_trees>1)
+
 # To dataframe
-df = pd.DataFrame(d_grammars)
+df = pd.DataFrame(d_grammar)
 
 # Move sentence to front column
 cols = sorted(list(df))
