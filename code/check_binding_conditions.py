1bca7ab480a3778ca8b1220566ac4ae6314df485
Emmanuel Chemla <emmanuel.chemla@free.fr>
2023-05-06 23:07:54 +0200

    fix binding conditions

--- code/check_binding_conditions.py
+++ code/check_binding_conditions.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 
+
 def get_agreement_match(row, role1, role2):
     agreement_match = {}
     for feature in ["GEN", "NUM", "ANIM"]:
@@ -8,8 +9,8 @@ def get_agreement_match(row, role1, role2):
     return agreement_match
 
 
-def calc_single_binding(row, pronoun_type: str):
-    # pronoun_type is from: ["PRO", "POSS"]
+def calc_single_binding(row, pronoun_position: str, target_position: str):
+    pronoun_type = np.nan
     agreement_match = np.nan
     valid_conditionA = np.nan
     valid_conditionB = np.nan
@@ -17,40 +18,36 @@ def calc_single_binding(row, pronoun_type: str):
     binding_quantifier = np.nan
     binding_pronoun = np.nan
 
-    subj_type, obj_type = row["subj_type"], row["obj_type"]
-    if row["poss_type"] == "subj":
-        subj_type = "POSS"
-    if row["poss_type"] == "obj":
-        obj_type = "POSS"
-
-    if subj_type == pronoun_type:
-        if not pd.isnull(row["obj_type"]):
-            agreement_match = get_agreement_match(row, "subj", "obj")
-            # valid_conditionA not relevant: no reflexive in subject position
-            valid_conditionB = True
-            if pronoun_type == "POSS":
-                valid_conditionC = True
+    if row[f"{pronoun_position}_type"] == "PRO":
+        pronoun_type = "PRO"
+        if not pd.isnull(row[f"{target_position}_type"]):
+            agreement_match = get_agreement_match(row, pronoun_position, target_position)
+            valid_conditionC = (pronoun_position == "subj")
+            if (pronoun_position == "obj") and row[f"{pronoun_position}_REFL"] == "true":
+                pronoun_type = "REFL"
+                valid_conditionA = True
             else:
-                valid_conditionC = False
-    if obj_type == pronoun_type:
-        agreement_match = get_agreement_match(row, "obj", "subj")
-        if row["obj_REFL"] == "true":
-            valid_conditionA = True
-        else:
-            valid_conditionB = False
-        valid_conditionC = True
-        if not pd.isnull(row["quantifier"]):
-        # Can we do something like: if row["subj_quantifier"] == "quantifier":
+                valid_conditionB = False
+        binding_quantifier = (not pd.isnull(row["quantifier"]))
+        binding_pronoun = (row[f"{target_position}_type"] == "PRO")
+
+    elif row[f"{pronoun_position}_type"] == "POSS":
+        pronoun_type = "POSS"
+        if not pd.isnull(row[f"{target_position}_type"]):
+            agreement_match = get_agreement_match(row, "poss_N", target_position)
+            valid_conditionC = True
+            valid_conditionB = True
             binding_quantifier = (not pd.isnull(row["quantifier"]))
-        if row["subj_type"] == "PRO":
-            binding_pronoun = True
+            binding_pronoun = (row[f"{target_position}_type"] == "PRO")
+
     res = {
         "agreement_match": agreement_match,
         "valid_conditionA": valid_conditionA,
         "valid_conditionB": valid_conditionB,
         "valid_conditionC": valid_conditionC,
         "binding_quantifier": binding_quantifier,
-        "binding_pronoun": binding_pronoun
+        "binding_pronoun": binding_pronoun,
+        "pronoun_type": pronoun_type
     }
     return res
 
@@ -59,9 +56,10 @@ def format_binding(bindings):
     res = {}
     for pronoun_type, binding in bindings.items():
         for prop, val in binding.items():
-            if prop == "agreement_match" and (not pd.isnull(val)):
-                for feature, val2 in val.items():
-                    res[f"{pronoun_type}_congruence_{feature}"] = val2
+            if prop == "agreement_match":
+                if not pd.isnull(val):
+                    for feature, val2 in val.items():
+                        res[f"{pronoun_type}_congruence_{feature}"] = val2
             else:
                 res[f"{pronoun_type}_{prop}"] = val
     return res
@@ -69,7 +67,7 @@ def format_binding(bindings):
 
 def calc_binding(row):
     bindings = {}
-    bindings["pro"] = calc_single_binding(row, pronoun_type="PRO")
-    bindings["poss"] = calc_single_binding(row, pronoun_type="POSS")
+    bindings["subj"] = calc_single_binding(row, pronoun_position="subj", target_position="obj")
+    bindings["obj"] = calc_single_binding(row, pronoun_position="obj", target_position="subj")
     res = format_binding(bindings)
     return res
